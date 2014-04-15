6209ac039f915d13c1102d80e93b794c66d53ad9
vidalle <vidalle@pcnt216-08.e-ima.ujf-grenoble.fr>
2014-04-15 13:27:44 +0200

    Creation package MVC pour la génération

---
+++ src/generation/controleur/Generateur.java
@@ -0,0 +1,19 @@
+package generation.controleur;
+
+import generation.modele.Module;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Generateur {
+
+	/**
+	 * 
+	 * @param listeModule
+	 * @param nomProjet
+	 * @param destination
+	 */
+	public void generer(ArrayList<Module> listeModule, String nomProjet, String destination){
+		
+	}
+}
