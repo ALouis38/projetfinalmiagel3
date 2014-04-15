c85c0251981a57b00b6efc298f5da36d050dd795
biboumx <biboumx@pcnt216-11.e-ima.ujf-grenoble.fr>
2014-04-15 16:17:36 +0200

    Interface de base (manque les "add event" associés)

--- src/generation/vue/box.java
+++ src/generation/vue/box.java
@@ -15,10 +15,15 @@ import javax.swing.JLabel;
 import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 public class box extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -49,6 +54,7 @@ public class box extends JFrame {
 		getContentPane().add(lblNexusX);
 		
 		JLabel lblNomDuProjet = new JLabel("Nom du projet:");
+		lblNomDuProjet.setForeground(Color.GRAY);
 		lblNomDuProjet.setBounds(12, 71, 105, 15);
 		getContentPane().add(lblNomDuProjet);
 		
@@ -58,6 +64,7 @@ public class box extends JFrame {
 		textField.setColumns(10);
 		
 		JLabel lblPathSave = new JLabel("Path Save:");
+		lblPathSave.setForeground(Color.GRAY);
 		lblPathSave.setBounds(12, 97, 105, 15);
 		getContentPane().add(lblPathSave);
 		
@@ -74,8 +81,42 @@ public class box extends JFrame {
 		glue_1.setBounds(201, 71, 1, 1);
 		getContentPane().add(glue_1);
 		
-		JButton btnBrowse = new JButton("Browse");
-		btnBrowse.setBounds(293, 97, 117, 15);
-		getContentPane().add(btnBrowse);
+		JLabel lblNombreDeClient = new JLabel("Nombre de client:");
+		lblNombreDeClient.setForeground(Color.GRAY);
+		lblNombreDeClient.setBounds(12, 128, 125, 15);
+		getContentPane().add(lblNombreDeClient);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(138, 126, 142, 19);
+		getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(79, 164, 3, 3);
+		getContentPane().add(scrollPane);
+		
+		JButton btnValider = new JButton("Valider");
+		btnValider.setBounds(328, 164, 84, 15);
+		getContentPane().add(btnValider);
+		
+		JLabel lblKernel = new JLabel("Kernel");
+		lblKernel.setBounds(12, 44, 51, 15);
+		getContentPane().add(lblKernel);
+		
+		JLabel lblModules = new JLabel("Modules");
+		lblModules.setBounds(12, 179, 70, 15);
+		getContentPane().add(lblModules);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(292, 97, 19, 15);
+		getContentPane().add(btnNewButton);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(8, 204, 129, 21);
+		getContentPane().add(menuBar);
+		
+		JList list = new JList();
+		list.setBounds(79, 213, 1, 1);
+		getContentPane().add(list);
 	}
 }
