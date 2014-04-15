dd1d3a18952c87ed4509145d775919064a520756
vidalle <vidalle@pcnt216-08.e-ima.ujf-grenoble.fr>
2014-04-15 13:56:09 +0200

    Generation des dossiers de base d'un nouveau projet

---
+++ src/generation/Main.java
@@ -0,0 +1,14 @@
+package generation;
+
+import generation.controleur.Generateur;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Generateur g = new Generateur();
+		g.generer(null, "testProjet1", "/home/v/vidalle/Projet-Nexus");
+
+	}
+
+}
