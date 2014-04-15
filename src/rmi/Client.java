52d77be070e0ea2d3e5036fbb2d83829bfa01ece
lafonth <lafonth@pcnt213-09.e-ima.ujf-grenoble.fr>
2014-04-15 12:43:57 +0200

    doudoux rmi

--- src/rmi/Client.java
+++ src/rmi/Client.java
@@ -1,6 +1,10 @@
 package rmi;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -9,15 +13,39 @@ import objetTest.ObjetTest;
 
 public class Client {
 	public static void main(String[] argv){
-		try {
-			// Récupère le registre
-			Registry registry = LocateRegistry.getRegistry(10000);
-			// Récupération de l'instance de l'objet distant
-			ObjetTest stub = (ObjetTest) registry.lookup("ObjetTest");
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (NotBoundException e) {
-			e.printStackTrace();
-		}
+
+		System.out.println("Lancement du client");
+	    if (System.getSecurityManager() == null) {
+	      System.setSecurityManager(new RMISecurityManager());
+	    }
+	    try {
+	      Remote r = Naming.lookup("rmi://127.0.0.1/TestRMI");
+	      System.out.println(r);
+	      if (r instanceof ObjetTest) {
+	        String s = ((ObjetTest) r).test();
+	        System.out.println("chaine renvoyee = " + s);
+	      }
+	    } catch (MalformedURLException e) {
+	      e.printStackTrace();
+	    } catch (RemoteException e) {
+	      e.printStackTrace();
+	    } catch (NotBoundException e) {
+	      e.printStackTrace();
+	    }
+	    System.out.println("Fin du client");
+		
+		
+		
+		
+//		try {
+//			// Récupère le registre
+//			Registry registry = LocateRegistry.getRegistry(10000);
+//			// Récupération de l'instance de l'objet distant
+//			ObjetTest stub = (ObjetTest) registry.lookup("ObjetTest");
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//		} catch (NotBoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
--- src/rmi/Serveur.java
+++ src/rmi/Serveur.java
@@ -1,6 +1,8 @@
 package rmi;
 
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -10,17 +12,43 @@ import objetTest.*;
 
 public class Serveur {
 
-	public static void main(String[] argv) {
+	public static void main(String[] argv) throws java.net.UnknownHostException{
+		
 		try {
-			// Crée une instance de l'objet distant : génération du stub
-			ObjetTest skeleton = (ObjetTest) UnicastRemoteObject.exportObject(
-					new ObjetTestImpl(), 10000);
-			// Crée le registre sur lequel se trouvera l'instance
-			Registry registry = LocateRegistry.createRegistry(10000);
-			// Publie l'instance sur le registre sous le nom "ObjetTest"
-			registry.rebind("ObjetTest", skeleton);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
+			
+			LocateRegistry.createRegistry(1099);
+
+		    System.out.println("Mise en place du Security Manager ...");	
+		    if (System.getSecurityManager() == null) {
+		      System.setSecurityManager(new RMISecurityManager());
+		    }
+		    
+		    ObjetTestImpl informationImpl = new ObjetTestImpl();
+		 
+		    String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/TestRMI";
+		    System.out.println("Enregistrement de l'objet avec l'url : " + url);
+		    Naming.rebind(url, informationImpl);
+
+		    System.out.println("Serveur lancé");
+		  } catch (RemoteException e) {
+		    e.printStackTrace();
+		  } catch (MalformedURLException e) {
+		    e.printStackTrace();
+		  }
+		
+		
+		
+		
+//		try {
+//			// Crée une instance de l'objet distant : génération du stub
+//			ObjetTest skeleton = (ObjetTest) UnicastRemoteObject.exportObject(
+//					new ObjetTestImpl(), 10000);
+//			// Crée le registre sur lequel se trouvera l'instance
+//			Registry registry = LocateRegistry.createRegistry(10000);
+//			// Publie l'instance sur le registre sous le nom "ObjetTest"
+//			registry.rebind("ObjetTest", skeleton);
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
