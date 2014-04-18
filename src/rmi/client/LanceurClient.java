781f929713abf3f7b0b2ecc29d7c80269ebfd591
louis <louis.a.ujf@gmail.com>
2014-04-18 10:21:09 +0200

    quickfix client

--- src/rmi/client/LanceurClient.java
+++ src/rmi/client/LanceurClient.java
@@ -1,24 +1,19 @@
 package rmi.client;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.rmi.RemoteException;
-
 import rmi.client.coreclient.Client;
-import rmi.serveur.coreserveur.GestFileInterface;
+
 
 public class LanceurClient {
 
 	public static void main(String[] args) {
-		Client c = new Client();
+		Client c = new Client(1099,"152.77.116.58");
 		
 		
 		//c.uploadFichierRegistry(1099, "152.77.116.58", "/home/a/amorel/git/projetfinalmiagel3/data/HubertLAFONTCV.pdf");
 		
 		//c.downloadFichierRegistry(1099, "152.77.116.58", "HubertLAFONTCV.pdf");
 		
-		c.supprimerFichier(1099, "152.77.116.58", "HubertLAFONTCV.pdf");
+		c.supprimerFichier("HubertLAFONTCV.pdf");
 		
 	}
 
--- src/rmi/client/coreclient/Client.java
+++ src/rmi/client/coreclient/Client.java
@@ -9,22 +9,27 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
-import rmi.serveur.coreserveur.GestFileInterface;
-import rmi.serveur.coreserveur.GestObjetInterface;
+import rmi.client.coreclient.GestFileInterface;
+import rmi.client.coreclient.GestObjetInterface;
 
 public class Client {
 	
-	public Remote getObjetRegistry(int port, String adresse, String nom){
+	private int port;
+	private String adresseServ;
+	
+	public Client(int p, String aS){
+		port = p;
+		adresseServ = aS;
+	}
+	
+	public Remote getObjetRegistry(String nom){
 		System.out.println("Lancement de la connexion");
 	      GestObjetInterface r = null;
 		try {
-			r = (GestObjetInterface) Naming.lookup("rmi://" + adresse+ ":" + port + "/gestObjet");
+			r = (GestObjetInterface) Naming.lookup("rmi://" + adresseServ+ ":" + port + "/gestObjet");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -35,7 +40,7 @@ public class Client {
 		
 	    System.out.println("Objet récupéré");
 	      try {
-			return r.getObjet(adresse, port, nom);
+			return r.getObjet(adresseServ, port, nom);
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,15 +49,15 @@ public class Client {
 
 	}
 	
-	public void addObjetRegistry(int port, String adresse, Remote obj){
+	public void addObjetRegistry(Remote obj){
 		System.out.println("Lancement de la connexion");
 		
 	    System.out.println("Objet envoyé");
 	}
 	
-	public void downloadFichierRegistry(int port, String adresseServ, String nomFichier){
+	public void downloadFichierRegistry(String nomFichier){
 		
-		GestFileInterface fileTest = (GestFileInterface) getObjetRegistry(port, adresseServ, "gestFile");
+		GestFileInterface fileTest = (GestFileInterface) getObjetRegistry("gestFile");
 		try {
 			byte[] tab = fileTest.downloadFile(nomFichier);
 			if(tab.length != 0){
@@ -79,11 +84,11 @@ public class Client {
 	}
 	
 	
-	public void uploadFichierRegistry(int port, String adresse, String chemin){
+	public void uploadFichierRegistry(String chemin){
 		System.out.println("Lancement de la connexion");
 		GestFileInterface r = null;
 	      try {
-				r = (GestFileInterface) Naming.lookup("rmi://" + adresse+ ":" + port + "/gestFile");
+				r = (GestFileInterface) Naming.lookup("rmi://" + adresseServ+ ":" + port + "/gestFile");
 				
 				File f = new File (chemin);
 				
@@ -117,7 +122,7 @@ public class Client {
 		    System.out.println("Objet renvoyé");
 	}
 	
-	public void supprimerFichier(int port, String adresseServ, String nomFichier){
+	public void supprimerFichier(String nomFichier){
 		try {
 			GestFileInterface gestFile = (GestFileInterface) Naming.lookup("rmi://" + adresseServ+ ":" + port + "/gestFile");
 			gestFile.supprFile(nomFichier);
