2d22bbe34a695b37171eff99fd9935f535c39efb
rugar <rugar@pcnt108-13.e-ima.ujf-grenoble.fr>
2014-04-18 13:51:29 +0200

    testation

--- src/rmi/client/LanceurClient.java
+++ src/rmi/client/LanceurClient.java
@@ -1,19 +1,23 @@
 package rmi.client;
 
 import rmi.client.coreclient.Client;
+import rmi.client.modulesclient.zip.ClientZip;
 
 
 public class LanceurClient {
 
 	public static void main(String[] args) {
 		Client c = new Client(1099,"152.77.116.58");
-		
+		ClientZip cZip = new ClientZip(1099, "152.77.116.58");
+		String[] fichiers =  {"cv.pdf","potion.jpg"};
+		String repertoire = "../data";
 		
 		//c.uploadFichierRegistry(1099, "152.77.116.58", "/home/a/amorel/git/projetfinalmiagel3/data/HubertLAFONTCV.pdf");
 		
 		//c.downloadFichierRegistry(1099, "152.77.116.58", "HubertLAFONTCV.pdf");
 		
 		c.supprimerFichier("HubertLAFONTCV.pdf");
+		cZip.compression(fichiers, repertoire);
 		
 	}
 
--- src/rmi/client/modulesclient/zip/ClientZip.java
+++ src/rmi/client/modulesclient/zip/ClientZip.java
@@ -6,25 +6,42 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import rmi.client.coreclient.Client;
+import rmi.serveur.modulesserveur.zip.GestZipImpl;
 
 public class ClientZip extends Client {
 
 	GestZipInterface zipI;
-	
+
 	public ClientZip(int p, String aS) {
 		super(p, aS);
 		try {
-			GestZipInterface gest = (GestZipInterface) Naming.lookup("rmi://" + this.adresseServ + ":" + this.port + "/gestZip");
+			GestZipInterface gest = (GestZipInterface) Naming.lookup("rmi://"
+					+ this.adresseServ + ":" + this.port + "/gestZip");
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void compression(String[] fichiers, String repertoire){
+		try {
+			zipI.compression(fichiers, repertoire);
+		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	public void decompression(String nomArchive, String repertoire){
+		try {
+			zipI.decompression(nomArchive, repertoire);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
